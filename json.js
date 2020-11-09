0789fd6ffa1b0e4143a55350d681b23518381146
JingshaLuo <70441839+JingshaLuo@users.noreply.github.com>
2020-11-09 18:58:45 +0800

    first commit

---
+++ json.js
@@ -0,0 +1,12 @@
+ data = {"quizcontent": [
+    {"question": "HTML: What does HTML stand for?", "correct": 2, "a1":"  Hyperlinks and Text Markup Language   ", "a2":"  Hyper Text Markup Language   ","a3":"  Home Tool Markup Language  ","a4":" not sure  "},
+    {"question": "HTML: Who is making the Web standards?", "correct": 4, "a1":" Google", "a2":" Mozilla ","a3":"  Microsoft   ","a4":"  The World Wide Web Consortium   "},
+	{"question": "HTML: Choose the correct HTML element for the largest heading: ",  "correct": 3, "a1":" h6 ", "a2":" h4  ","a3":" h1  ","a4":"  P "},
+    {"question": "CSS: What does CSS stand for ? ", "correct": 2, "a1":"Computer Style Sheets", "a2":"Cascading Style Sheets","a3":"Colorful Style Sheets","a4":"Creative Style Sheets"},
+	{"question": "CSS: Which HTML attribute is used to define inline styles? ", "correct": 1, "a1":"style", "a2":"font","a3":"styles","a4":"class"},
+	{"question": "CSS: Which property is used to change the background color? ", "correct": 4, "a1":"bc", "a2":"color","a3":"bgcolor","a4":"background-color"},
+    {"question": "Bootstrap: The Bootstrap grid system is based on how many columns? ", "correct": 4, "a1":"3", "a2":"6","a3":"9","a4":"12"},
+    {"question": "Bootstrap: Which class provides a responsive fixed width container?  ", "correct": 2, "a1":".container-fixed", "a2":".container","a3":".container-fluid","a4":"container"}
+    ],
+	
+};
